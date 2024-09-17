4ac838eff8b6b8b19f24bca281b7e54e5fd0d11e
kleiber123456 <165842462+kleiber123456@users.noreply.github.com>
2024-09-17 18:28:05 -0500

    Update productoServicio.js/dfef

--- models/productoServicio.js
+++ models/productoServicio.js
@@ -1,30 +1,31 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
 const productoServicioSchema = mongoose.Schema({
-  _id:{ 
-    type: String,
-    required:true,
-    unique: true 
-  },
-  Nombre:{ 
-    type: String,
-    required:true
-  },
-  Descripcion:{
-    type: String,
-    required:true
-  },
-  Precio:{
-    type: Number,
-    required: true,
-  },
-  Tipo:{
-    type: String,
-    required: true,
-    enum: ['Producto', 'Servicio'], 
-    message: 'El tipo debe ser "Producto" o "Servicio".'
-  }
-})
+    _id: { 
+        type: String,
+        required: true,
+        unique: true 
+    },
+    Nombre: { 
+        type: String,
+        required: true
+    },
+    Descripcion: {
+        type: String,
+        required: true
+    },
+    Precio: {
+        type: Number,
+        required: true
+    },
+    Tipo: {
+        type: String,
+        required: true,
+        enum: ['Producto', 'Servicio'], 
+        message: 'El tipo debe ser "Producto" o "Servicio".'
+    }
+});
 
-productoServicio = mongoose.model("ProductoServicio",productoServicioSchema);
+const ProductoServicio = mongoose.model('ProductoServicio', productoServicioSchema);
 
-module.exports = productoServicio
+module.exports = ProductoServicio;
