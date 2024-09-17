eb058ad4398c9004b03caddc98d3ea6a741b5c8c
kleiber123456 <165842462+kleiber123456@users.noreply.github.com>
2024-09-17 18:28:36 -0500

    Update ventas.js/sfwr

--- models/ventas.js
+++ models/ventas.js
@@ -2,30 +2,29 @@ const mongoose = require("mongoose");
 
 // Define el esquema para "Ventas"
 const ventasSchema = mongoose.Schema({
-    _id:{ 
+    _id: { 
         type: String,
-        required:true,
+        required: true,
         unique: true 
     },
-  
     Clientes_id: {
-    type: mongoose.Schema.Types.String,
-    ref: 'Clientes', // Nombre del modelo al que hace referencia
-    required: true
-  },
-  FechaVenta: {
-    type: Date,
-    required: true
-  },
-  Total: {
-    type: Number,
-    required: true
-  },
-  ProductoServicio_id: {
-    type: mongoose.Schema.Types.String,
-    ref: 'ProductoServicios', // Nombre del modelo al que hace referencia
-    required: true
-  }
+        type: mongoose.Schema.Types.String,
+        ref: 'Clientes',
+        required: true
+    },
+    FechaVenta: {
+        type: Date,
+        required: true
+    },
+    Total: {
+        type: Number,
+        required: true
+    },
+    ProductoServicio_id: {
+        type: mongoose.Schema.Types.String,
+        ref: 'ProductoServicio', // Asegúrate de usar el nombre correcto del modelo
+        required: true
+    }
 });
 
 module.exports = mongoose.model('Ventas', ventasSchema);
